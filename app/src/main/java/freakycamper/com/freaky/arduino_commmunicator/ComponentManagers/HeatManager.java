8b08771be8a3da6abde816e87cdf265bfff20522
Freakstreet ADS pc <freakstreet@laposte.net>
2016-10-17 11:21:08 +0200

    Corrected dialog management from parent dialog

--- app/src/main/java/freakycamper/com/freaky/arduino_commmunicator/ComponentManagers/HeatManager.java
+++ app/src/main/java/freakycamper/com/freaky/arduino_commmunicator/ComponentManagers/HeatManager.java
@@ -1,6 +1,7 @@
 package freakycamper.com.freaky.arduino_commmunicator.ComponentManagers;
 
 import android.content.Context;
+import android.content.DialogInterface;
 
 import freakycamper.com.freaky.arduino_commmunicator.campdatas.HeatItem;
 import freakycamper.com.freaky.arduino_commmunicator.campdatas.TemperatureItem;
@@ -12,7 +13,6 @@ import freakycamper.com.freaky.arduino_commmunicator.dialog.DialogHeat;
  */
 public class HeatManager extends MainManager {
 
-    private DialogHeat _dialog;
     private HeatItem _heat;
     private float[] _temps;
 
@@ -32,14 +32,18 @@ public class HeatManager extends MainManager {
         });
 
         _temps = new float[2];
-        _dialog = null;
         _heat = new HeatItem(HeatItem.eHeatModuleState.HEAT_MODULE_OFF, -99, (char)0, (char)0);
     }
 
     public void showDialog(Context context){
-        _dialog = new DialogHeat(context, this, _temps);
-        _dialog.updateHeatDialog(_temps);
-        _dialog.show();
+        correspondingDialog = new DialogHeat(context, this, _temps);
+        correspondingDialog.setOnDismissListener(new DialogInterface.OnDismissListener() {
+            @Override
+            public void onDismiss(DialogInterface dialog) {
+                correspondingDialog = null;
+            }
+        });
+        correspondingDialog.show();
     }
 
     public HeatItem getHeatParams(){
@@ -56,18 +60,17 @@ public class HeatManager extends MainManager {
         _temps[1] = TemperatureItem.getTemperatureFromType(tempsTm, TemperatureItem.eTemperatureType.TEMP_INSIDE2);
 
         if ((t1 != _temps[0]) || (t2 != _temps[1])){
-            if (_dialog != null){
-                _dialog.updateHeatDialog(_temps);
+            if (correspondingDialog != null){
+                ((DialogHeat)correspondingDialog).updateHeatDialog(_temps);
             }
         }
-
     }
 
     @Override
     public void updateFromTM(char[] tm){
         _heat = new HeatItem(HeatItem.eHeatModuleState.values()[tm[3]], CampDuinoProtocol.decodeFloatFromTm(tm[4], tm[5]), tm[6], tm[7]);
-        if (_dialog != null){
-            _dialog.updateFromTm(_heat);
+        if (correspondingDialog != null){
+            ((DialogHeat)correspondingDialog).updateFromTm(_heat);
             _heat.updateFanVal( tm[4], true);
             _heat.updateFanVal( tm[5], false);
         }
