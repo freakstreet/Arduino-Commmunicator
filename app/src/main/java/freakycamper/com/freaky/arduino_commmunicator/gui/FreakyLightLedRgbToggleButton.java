9b4f33e97a131c51662401fd9e4769a32eb4eb37
Freakstreet ADS pc <freakstreet@laposte.net>
2016-10-20 14:33:54 +0200

    Changed color picker to FlaskColorPicker

--- app/src/main/java/freakycamper/com/freaky/arduino_commmunicator/gui/FreakyLightLedRgbToggleButton.java
+++ app/src/main/java/freakycamper/com/freaky/arduino_commmunicator/gui/FreakyLightLedRgbToggleButton.java
@@ -1,6 +1,7 @@
 package freakycamper.com.freaky.arduino_commmunicator.gui;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
@@ -8,8 +9,13 @@ import android.util.AttributeSet;
 import android.widget.CompoundButton;
 import android.widget.ToggleButton;
 
+import freakycamper.com.freaky.arduino_commmunicator.R;
 import freakycamper.com.freaky.arduino_commmunicator.campdatas.LightItem;
 import freakycamper.com.freaky.arduino_commmunicator.dialog.DialogPickColor;
+import freakycamper.com.freaky.arduino_commmunicator.gui.FlaskColorPicker.ColorPickerView;
+import freakycamper.com.freaky.arduino_commmunicator.gui.FlaskColorPicker.OnColorSelectedListener;
+import freakycamper.com.freaky.arduino_commmunicator.gui.FlaskColorPicker.builder.ColorPickerClickListener;
+import freakycamper.com.freaky.arduino_commmunicator.gui.FlaskColorPicker.builder.ColorPickerDialogBuilder;
 import freakycamper.com.freaky.arduino_commmunicator.utils.FontUtils;
 
 
@@ -72,6 +78,56 @@ public class FreakyLightLedRgbToggleButton extends ToggleButton implements Dialo
 
     }
 
+
+    public void pickRGBColor(){
+
+        final Context context = getContext();
+
+        ColorPickerDialogBuilder
+                .with(context, R.style.AppTheme)
+                .setTitle("Select light desired color")
+                .initialColor(Color.rgb(item.getRedValue(), item.getGreenValue(), item.getBlueValue()))
+                .wheelType(ColorPickerView.WHEEL_TYPE.FLOWER)
+                .density(12)
+                .showColorEdit(false)
+                .showAlphaSlider(false)
+                .setOnColorSelectedListener(new OnColorSelectedListener() {
+                    @Override
+                    public void onColorSelected(int selectedColor) {
+                        setBackgroundColor(selectedColor);
+                    }
+                })
+                .setPositiveButton("ok", new ColorPickerClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int selectedColor, Integer[] allColors) {
+                        item.updateLightRGBStatus((char)Color.red(selectedColor), (char)Color.green(selectedColor), (char)Color.blue(selectedColor));
+                        setBackgroundColor(selectedColor);
+                        if (allColors != null) {
+                            StringBuilder sb = null;
+
+                            for (Integer color : allColors) {
+                                if (color == null)
+                                    continue;
+                                if (sb == null)
+                                    sb = new StringBuilder("Color List:");
+                                sb.append("\r\n#" + Integer.toHexString(color).toUpperCase());
+                            }
+
+                        }
+                    }
+                })
+                .setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                    }
+                })
+                //.setColorEditTextColor(ContextCompat.getColor(SampleActivity.this, android.R.color.holo_blue_bright))
+                .build()
+                .show();
+
+
+    }
+
     @Override
     public boolean performClick() {
         if (item==null)
@@ -84,10 +140,7 @@ public class FreakyLightLedRgbToggleButton extends ToggleButton implements Dialo
                 break;
 
             case RGB_DIMMER:
-                DialogPickColor dlg = new DialogPickColor(getContext());
-                dlg.setLightColor(Color.rgb(item.getRedValue(), item.getGreenValue(), item.getBlueValue()));
-                dlg.setListener(this);
-                dlg.show();
+                pickRGBColor();
                 break;
 
             case DIMMER:
