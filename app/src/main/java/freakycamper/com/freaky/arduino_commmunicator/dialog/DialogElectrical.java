de55514438b75cdeb8ca4a3b33eac2fde840a5b2
Freakstreet ADS pc <freakstreet@laposte.net>
2016-10-21 15:47:00 +0200

    Added textviews click listeners

--- app/src/main/java/freakycamper/com/freaky/arduino_commmunicator/dialog/DialogElectrical.java
+++ app/src/main/java/freakycamper/com/freaky/arduino_commmunicator/dialog/DialogElectrical.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.TextView;
 
 import freakycamper.com.freaky.arduino_commmunicator.ComponentManagers.ColdManager;
 import freakycamper.com.freaky.arduino_commmunicator.ComponentManagers.ElectricalManager;
@@ -31,74 +32,98 @@ public class DialogElectrical extends DialogPopUpDelayed implements ElectricalMa
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND);
 
         CircleButton b;
-        b = (CircleButton)findViewById(R.id.bt_relay_water);
-        lbl = context.getText(R.string.elec_water_module).toString();
-        customizeButton(b, lbl);
+        TextView t;
 
-        b.setOnClickListener(new View.OnClickListener() {
+        b = (CircleButton)findViewById(R.id.bt_relay_water);
+        t = (TextView)findViewById(R.id.text_relay_water) ;
+        setMonitoredComponent(b);
+        setMonitoredComponent(t);
+
+        View.OnClickListener ocl = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 elec.getSendTcListener().sendTC(CampDuinoProtocol.buildSwitchRelayTC(CampDuinoProtocol.eProtTcSwitch.PROT_SWITCH_WATER_MODULE, !elec.getRelayStatus(ElectricalItem.eRelayType.R_WATER)));
             }
-        });
+        };
+
+        b.setOnClickListener(ocl);
+        t.setOnClickListener(ocl);
+
+
 
         b = (CircleButton)findViewById(R.id.bt_relay_cold);
-        lbl = context.getText(R.string.elec_cold_module).toString();
-        customizeButton(b, lbl);
-        b.setOnClickListener(new View.OnClickListener() {
+        t = (TextView)findViewById(R.id.text_relay_cold) ;
+        setMonitoredComponent(b);
+        setMonitoredComponent(t);
+
+        ocl = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                boolean status = elec.getRelayStatus(ElectricalItem.eRelayType.R_COLD);
-                elec.getSendTcListener().sendTC(CampDuinoProtocol.buildSwitchRelayTC(CampDuinoProtocol.eProtTcSwitch.PROT_SWITCH_COLD_MODULE, !status));
-                if (!status){
-                    // sends the default temperature to manage the fridge
-                    elec.getSendTcListener().sendTC(CampDuinoProtocol.buildSetFridgeConsigne(ColdManager.DEFAULT_TEMP_FRIDGE));
-                }
+                elec.getSendTcListener().sendTC(CampDuinoProtocol.buildSwitchRelayTC(CampDuinoProtocol.eProtTcSwitch.PROT_SWITCH_COLD_MODULE, !elec.getRelayStatus(ElectricalItem.eRelayType.R_COLD)));
             }
-        });
+        };
+        b.setOnClickListener(ocl);
+        t.setOnClickListener(ocl);
+
 
         b = (CircleButton)findViewById(R.id.bt_relay_heater);
-        lbl = context.getText(R.string.elec_heat_module).toString();
-        customizeButton(b, lbl);
-        b.setOnClickListener(new View.OnClickListener() {
+        t = (TextView)findViewById(R.id.text_relay_heater);
+        setMonitoredComponent(b);
+        setMonitoredComponent(t);
+
+        ocl = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // heater button is only used to show relay status, not direct action outside the heater dialog
                 elec.getSendTcListener().sendTC(CampDuinoProtocol.buildSwitchRelayTC(CampDuinoProtocol.eProtTcSwitch.PROT_SWITCH_HEAT_MODULE, !elec.getRelayStatus(ElectricalItem.eRelayType.R_HEATER)));
             }
-        });
+        };
+        b.setOnClickListener(ocl);
+        t.setOnClickListener(ocl);
 
         b = (CircleButton)findViewById(R.id.bt_relay_light);
-        lbl = context.getText(R.string.elec_light_module).toString();
-        customizeButton(b, lbl);
-        b.setOnClickListener(new View.OnClickListener() {
+        t = (TextView)findViewById(R.id.text_relay_light);
+        setMonitoredComponent(b);
+        setMonitoredComponent(t);
+
+        ocl = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 elec.getSendTcListener().sendTC(CampDuinoProtocol.buildSwitchRelayTC(CampDuinoProtocol.eProtTcSwitch.PROT_SWITCH_LIGHT_MODULE, !elec.getRelayStatus(ElectricalItem.eRelayType.R_LIGHT)));
             }
-        });
+        };
+        b.setOnClickListener(ocl);
+        t.setOnClickListener(ocl);
 
         b = (CircleButton)findViewById(R.id.bt_relay_aux);
-        lbl = context.getText(R.string.elec_aux_module).toString();
-        customizeButton(b, lbl);
-        b.setOnClickListener(new View.OnClickListener() {
+        t = (TextView)findViewById(R.id.text_relay_aux);
+        setMonitoredComponent(b);
+        setMonitoredComponent(t);
+
+        ocl = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 elec.getSendTcListener().sendTC(CampDuinoProtocol.buildSwitchRelayTC(CampDuinoProtocol.eProtTcSwitch.PROT_SWITCH_AUX_MODULE, !elec.getRelayStatus(ElectricalItem.eRelayType.R_AUX)));
             }
-        });
+        };
+        b.setOnClickListener(ocl);
+        t.setOnClickListener(ocl);
 
         b = (CircleButton)findViewById(R.id.bt_relay_spare);
-        b.setColor(getContext().getResources().getColor(android.R.color.darker_gray));
-        b.setImageDrawable(null);
-        lbl = context.getText(R.string.elec_spare_module).toString();
-        customizeButton(b, lbl);
-        b.setOnClickListener(new View.OnClickListener() {
+        t = (TextView)findViewById(R.id.text_relay_spare);
+        setMonitoredComponent(b);
+        setMonitoredComponent(t);
+
+        ocl = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 elec.getSendTcListener().sendTC(CampDuinoProtocol.buildSwitchRelayTC(CampDuinoProtocol.eProtTcSwitch.PROT_SWITCH_SPARE_MODULE, !elec.getRelayStatus(ElectricalItem.eRelayType.R_SPARE)));
             }
-        });
+        };
+        b.setOnClickListener(ocl);
+        t.setOnClickListener(ocl);
+
+        b.setColor(getContext().getResources().getColor(android.R.color.darker_gray));
+        b.setImageDrawable(null);
 
         this.setOnDismissListener(new OnDismissListener() {
             public void onDismiss(DialogInterface dialog) {
@@ -111,15 +136,6 @@ public class DialogElectrical extends DialogPopUpDelayed implements ElectricalMa
 
     private void removeManagerEventListener(){
         elec.removeRelayModuleListener(this);
-    }
-
-    private void customizeButton(CircleButton b, String lbl){
-        Context ctx= getContext();
- /*       b.setTypeface(FontUtils.loadFontFromAssets(ctx, FontUtils.FONT_DOSIS_MEDIUM));
-        b.setText(lbl);
-        b.setTextSize(20);
-        b.setTextOn(lbl + "\n" + ctx.getText(R.string.elec_activated_module));
-        b.setTextOff(lbl + "\n" + ctx.getText(R.string.elec_desactivated_module));*/
     }
 
     public void updateGui(){
